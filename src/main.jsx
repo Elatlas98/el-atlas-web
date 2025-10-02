2ce28d62ffe33d23809c4a4ef25fbda7b82a7360
Elatlas98 <abdelilahelmassnaoui@gmail.com>
2025-10-03 00:41:33 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ElAtlasLanding from './ElAtlasLanding'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ElAtlasLanding />
+  </React.StrictMode>,
+)
