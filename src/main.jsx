014af240a908921671a68807e30d9160c1675068
Elatlas98 <abdelilahelmassnaoui@gmail.com>
2025-10-03 00:26:52 +0200

    Delete src logo

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import ElAtlasLanding from './ElAtlasLanding.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ElAtlasLanding />
-  </React.StrictMode>
-)
